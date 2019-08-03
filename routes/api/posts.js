291cd14cda01e98b41b9fa647562a262709f8de1
Subho Karmakar <isubhokarma@gmail.com>
2019-08-03 07:28:23 +0530

    Unlike a post API

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -147,4 +147,35 @@ router.put("/like/:id", auth, async (req, res) => {
   }
 });
 
+// @route   PUT api/posts/unlike/:id
+// @desc    unLike a post
+// @access  Private
+router.put("/unlike/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check if the post been liked by the particular id user
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length ===
+      0
+    ) {
+      return res.status(400).json({ msg: "Post has not yet been liked" });
+    }
+
+    // Get remove index
+    const removeIndex = post.likes
+      .map(like => like.user.toString())
+      .indexOf(req.user.id);
+
+    post.likes.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
 module.exports = router;
